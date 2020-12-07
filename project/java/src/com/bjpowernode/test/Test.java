91194757ecf01222bcddbae14599a04de938836b
zs <zs@bjpowernode.com>
2020-12-07 21:42:08 +0800

    更新

--- project/java/src/com/bjpowernode/test/Test.java
+++ project/java/src/com/bjpowernode/test/Test.java
@@ -7,5 +7,6 @@ package com.bjpowernode.test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("更新");
     }
 }
