573502fc7d3bd62b7649183885f78b51596c8905
zs <zs@bjpowernode.com>
2020-12-07 21:40:39 +0800

    完整项目

---
+++ project/testWeb/src/java/com/bjpowernode/test/MyTest.java
@@ -0,0 +1,11 @@
+package com.bjpowernode.test;
+
+/**
+ * @author 栗舜顺
+ * 2020/12/7 0007
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        System.out.println("web");
+    }
+}
